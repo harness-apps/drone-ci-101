001a8e192982aa082c9f8fded1407862fd3e8125
Kamesh <kamesh.sampath@hotmail.com>
2022-10-11 19:28:02 +0530

    Updated with Drone CI Docker Extension Instructions

--- hack/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-drone exec --trusted --env-file=.envrc.local --pipeline=delete-cluster
-docker network rm "${DOCKER_NETWORK_NAME}" || true
-rm -rf "$KUBECONFIG"
